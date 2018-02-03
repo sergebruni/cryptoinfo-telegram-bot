4ddac18ca64ac742d7889db7a6b6d03a0ed7d78f
Sergio Bruni <sergebruni@gmail.com>
2018-02-03 00:22:53 -0400

    Adding early exits & using const

--- bot.js
+++ bot.js
@@ -6,10 +6,10 @@ const jsonFile    = require('jsonfile');
 const CronJob     = require('cron').CronJob;
 
 // Load config file
-let config        = require("./config.json");
+const config      = require("./config.json");
 
 // Create Twitter API REST Client
-let client = new Twitter(config.twitter); 
+const client = new Twitter(config.twitter); 
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(config.telegram.token, {polling: true});
@@ -17,33 +17,33 @@ const bot = new TelegramBot(config.telegram.token, {polling: true});
 // Listen for command messages.
 bot.on('message', (msg) => { 
   // Last Tweet action
-  if (msg.entities[0].type === 'bot_command'){
-    switch (msg.text) {
-      case '/start':
-      case '/start@cryptoinfotelegrambot':
-        bot.sendMessage(msg.chat.id, "Welcome, type /help for a command list");
-        registerChat(msg.chat.id)
-        sendTweet(msg.chat.id)
-        break;
-      case '/help':
-      case '/help@cryptoinfotelegrambot':
-        let text = `/start - Start bot show commands
-        /help - Show bot help info
-        /lasttweet - Send Last Tweet`;
-        bot.sendMessage(msg.chat.id, text);
-        break;
-      case '/lasttweet':
-      case '/lasttweet@cryptoinfotelegrambot':
-        sendTweet(msg.chat.id)
-        break;
-    }
-  }  
+  if (msg.entities[0].type !== 'bot_command'){ return }
+
+  switch (msg.text) {
+    case '/start':
+    case '/start@cryptoinfotelegrambot':
+      bot.sendMessage(msg.chat.id, "Welcome, type /help for a command list");
+      registerChat(msg.chat.id)
+      sendTweet(msg.chat.id)
+      break;
+    case '/help':
+    case '/help@cryptoinfotelegrambot':
+      const text = `  /start - Start bot show commands
+      /help - Show bot help info
+      /lasttweet - Send Last Tweet`;
+      bot.sendMessage(msg.chat.id, text);
+      break;
+    case '/lasttweet':
+    case '/lasttweet@cryptoinfotelegrambot':
+      sendTweet(msg.chat.id)
+      break;
+  }
 });
 
 // Send Last Tweet function.
 const sendTweet = (chatId) => {
   if (!_.isUndefined(config.app.lastTweet.text)) {
-    let text = `${config.app.lastTweet.text}
+    const text = `${config.app.lastTweet.text}
     at ${config.app.lastTweet.created_at}`;
     bot.sendMessage(chatId, text);
   }
@@ -53,34 +53,28 @@ const sendTweet = (chatId) => {
 
 // Get tweet function through Twitter API
 const getTweets = (chatId) => {  
-  let params = { screen_name: 'cryptochoe', exclude_replies: true };
+  const params = { screen_name: 'cryptochoe', exclude_replies: true };
 
   client.get('statuses/user_timeline', params, function(error, tweets, response) {
-    if (!error) {
-      tweets = _.orderBy(tweets, ['id_str'], ['asc'])
-      
-      if (config.app.since_id < tweets[tweets.length - 1].id) {
-        config.app.lastTweet = tweets[tweets.length - 1];
-        config.app.since_id = config.app.lastTweet.id;
+    if (error) return console.log(error)
 
-        saveConfigFile(); 
-        sendTweet(chatId);
-      }    
-      else 
-        console.log('nothing to update')
+    tweets = _.orderBy(tweets, ['id_str'], ['asc'])
+    
+    if (config.app.since_id >= tweets[tweets.length - 1].id) return console.log('nothing to update')
       
-    }
-    else {
-      console.log(error)
-    }
+    config.app.lastTweet = tweets[tweets.length - 1];
+    config.app.since_id = config.app.lastTweet.id;
+
+    saveConfigFile(); 
+    sendTweet(chatId);              
+
   });
 }
 
 const registerChat = (chatId) => {
-  if (_.indexOf(config.app.chatIdArr, chatId) === -1){
-    config.app.chatIdArr.push(chatId);
-    saveConfigFile(); 
-  }
+  if (_.indexOf(config.app.chatIdArr, chatId) !== -1){ return }  
+  config.app.chatIdArr.push(chatId);
+  saveConfigFile();   
 }
 
 // Cron job to stay updated to Tweets
