a46d4b3cfaaa598b7334f78996882e8ab9320330
Sergio Bruni <sergebruni@gmail.com>
2018-02-02 23:47:48 -0400

    added command support, added cronjob to get last updates from CryptoChoe

--- bot.js
+++ bot.js
@@ -3,9 +3,10 @@ const Twitter     = require('twitter');
 
 const _           = require('lodash');
 const jsonFile    = require('jsonfile');
+const CronJob     = require('cron').CronJob;
 
 // Load config file
-let config   = require("./config.json");
+let config        = require("./config.json");
 
 // Create Twitter API REST Client
 let client = new Twitter(config.twitter); 
@@ -13,48 +14,61 @@ let client = new Twitter(config.twitter);
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(config.telegram.token, {polling: true});
 
-// Send message on start command, show bot commands keyboard
-bot.onText(/\/start/, (msg) => {
-  bot.sendMessage(msg.chat.id, "Welcome", {
-    "reply_markup": {
-      "keyboard": [["Last Tweet"]]
-    }
-  });
-});
-
-// Listen for selected messages.
+// Listen for command messages.
 bot.on('message', (msg) => { 
   // Last Tweet action
-  if (msg.text.indexOf("Last Tweet") === 0) {
-    if (_.isUndefined(config.app.lastTweet)) {
-      let text  = config.app.lastTweet.text + '\n';
-          text += 'at '+ config.app.lastTweet.created_at + '\n';
-      bot.sendMessage(msg.chat.id, text);
+  if (msg.entities[0].type === 'bot_command'){
+    switch (msg.text) {
+      case '/start':
+      case '/start@cryptoinfotelegrambot':
+        bot.sendMessage(msg.chat.id, "Welcome, type /help for a command list");
+        registerChat(msg.chat.id)
+        sendTweet(msg.chat.id)
+        break;
+      case '/help':
+      case '/help@cryptoinfotelegrambot':
+        let text = `/start - Start bot show commands
+        /help - Show bot help info
+        /lasttweet - Send Last Tweet`;
+        bot.sendMessage(msg.chat.id, text);
+        break;
+      case '/lasttweet':
+      case '/lasttweet@cryptoinfotelegrambot':
+        sendTweet(msg.chat.id)
+        break;
     }
-    else
-      getTweets(msg.chat.id)
-  }
+  }  
 });
 
-function getTweets(chatId = undefined) {
-  
+// Send Last Tweet function.
+const sendTweet = (chatId) => {
+  if (!_.isUndefined(config.app.lastTweet.text)) {
+    let text = `${config.app.lastTweet.text}
+    at ${config.app.lastTweet.created_at}`;
+    bot.sendMessage(chatId, text);
+  }
+  else
+    getTweets(chatId)
+}
+
+// Get tweet function through Twitter API
+const getTweets = (chatId) => {  
   let params = { screen_name: 'cryptochoe', exclude_replies: true };
 
   client.get('statuses/user_timeline', params, function(error, tweets, response) {
     if (!error) {
-
       tweets = _.orderBy(tweets, ['id_str'], ['asc'])
+      
+      if (config.app.since_id < tweets[tweets.length - 1].id) {
+        config.app.lastTweet = tweets[tweets.length - 1];
+        config.app.since_id = config.app.lastTweet.id;
 
-      let text  = tweets[tweets.length - 1].text + '\n';
-          text += 'at '+ tweets[tweets.length - 1].created_at + '\n';
-
-      bot.sendMessage(chatId, text);
-
-      config.app.lastTweet = tweets[tweets.length - 1];
-      config.app.since_id = config.app.lastTweet.id;
-
-      saveConfigFile();
-    
+        saveConfigFile(); 
+        sendTweet(chatId);
+      }    
+      else 
+        console.log('nothing to update')
+      
     }
     else {
       console.log(error)
@@ -62,7 +76,28 @@ function getTweets(chatId = undefined) {
   });
 }
 
-function saveConfigFile() {
-  jsonFile.writeFileSync(__dirname + "/config.json", config);
+const registerChat = (chatId) => {
+  if (_.indexOf(config.app.chatIdArr, chatId) === -1){
+    config.app.chatIdArr.push(chatId);
+    saveConfigFile(); 
+  }
+}
+
+// Cron job to stay updated to Tweets
+const job = new CronJob({
+  cronTime: "*/5 * * * *",
+  onTick: function() {
+    console.log('cron job runing')
+    _.forEach(config.app.chatIdArr, (chatId, key) => {
+      getTweets(chatId);
+    });    
+  },
+  runOnInit: true,
+  timeZone: 'America/Caracas'
+});
+
+// Save new config file values
+const saveConfigFile = () => {
+  jsonFile.writeFileSync("./config.json", config);
   console.log('config file saved')
 }
