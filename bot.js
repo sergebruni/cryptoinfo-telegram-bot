1fbc5e32b6cac459f9384456793618d2bbe5a02e
Sergio Bruni <sergebruni@gmail.com>
2018-02-18 13:46:42 -0400

    updated bot logic into modules

--- bot.js
+++ bot.js
@@ -1,24 +1,26 @@
-const TelegramBot = require('node-telegram-bot-api')
-const Twitter     = require('twitter')
+const TelegramBot    = require('node-telegram-bot-api')
 
-const _           = require('lodash')
-const jsonFile    = require('jsonfile')
-const CronJob     = require('cron').CronJob
+const TwitterService = require('./src/twitterService/twitterService')
+const Cron           = require('./src/cron')
 
 // Load config file
-const config      = require('./config.json')
+const config         = require('./config.json')
 
-// Load commands
-const commands = require('./src/commands/index')
-
-// Create Twitter API REST Client
-const twitter = new Twitter(config.twitter);
+// Create TwitterService client
+const twitter = new TwitterService(config.twitter)
 
 // Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(config.telegram.token, {polling: true});
+const bot = new TelegramBot(config.telegram.token, {polling: true})
+
+// Load commands
+const commands       = require('./src/commands/index')
 
 // Create botan.io client for bot statistics tracking
-const Botan = require('botanio')(config.botan.token);
+const Botan = require('botanio')(config.botan.token)
+
+// Create Cron client
+const cronJob = new Cron(twitter)
+cronJob.run()
 
 // Listen for command messages.
 bot.on('message', (msg) => { 
@@ -29,15 +31,18 @@ bot.on('message', (msg) => {
   switch (cmd) {
     case '/start':
     case '/start@cryptoinfotelegrambot':
-      handleStart(msg.chat.id)
+      commands['start'].run(bot, twitter, msg.chat.id)
+      Botan.track(msg, 'start')
       break;    
     case '/stop':
     case '/stop@cryptoinfotelegrambot':
-      handleStop(msg.chat.id)
+      commands['stop'].run(bot, msg.chat.id)
+      Botan.track(msg, 'stop')
       break;   
     case '/lasttweet':
     case '/lasttweet@cryptoinfotelegrambot':
-      handleLastTweet(msg.chat.id)
+      commands['lasttweet'].run(bot, twitter, msg.chat.id)
+      Botan.track(msg, 'lasttweet')
       break;
     case '/pricecheck':
     case '/pricecheck@cryptoinfotelegrambot':
@@ -58,106 +63,4 @@ bot.on('message', (msg) => {
       bot.sendMessage(msg.chat.id, `command not recognized, type /help for a list of commands`);
       break;
   }
-});
-
-// Send Last Tweet function.
-const sendTweet = (chatId) => {
-  if (!_.isUndefined(config.app.lastTweet.text)) {
-    const text = `${config.app.lastTweet.text}
-    at ${config.app.lastTweet.created_at}`;
-    bot.sendMessage(chatId, text);
-  }
-  else {
-    getTweets().then((res) => sendTweet(chatId)).catch((err) => console.log(err))
-  }  
-}
-
-// Get tweet promise through Twitter API
-const getTweets = () => new Promise(
-  function (resolve, reject) {
-    const params = { screen_name: 'cryptochoe', exclude_replies: true };
-
-    twitter.get('statuses/user_timeline', params, (error, tweets, response) => {
-      if (error) return reject(error)
-
-      tweets = _.orderBy(tweets, ['id_str'], ['asc'])
-      
-      if (config.app.since_id >= tweets[tweets.length - 1].id) 
-        return reject({
-          curr_id: tweets[tweets.length - 1].id,
-          last_id: config.app.since_id,
-          message: 'nothing to update'
-        })
-        
-      config.app.lastTweet = tweets[tweets.length - 1];
-      config.app.since_id = config.app.lastTweet.id;
-
-      saveConfigFile()
-      return resolve({success: true, message: 'feed updated'})
-    });
-  }
-);
-
-// Register chat id function
-const manageChats = (chatId, register = true) => {
-  console.log(chatId)
-  if (register) {
-    if (_.indexOf(config.app.chatIdArr, chatId) !== -1){ return }
-    config.app.chatIdArr.push(chatId)
-  }
-  else {
-    if (_.indexOf(config.app.chatIdArr, chatId) === -1){ return }
-    const idx = config.app.chatIdArr.indexOf(chatId)
-    config.app.chatIdArr.splice(idx, 1)
-  }   
-  
-  saveConfigFile();   
-}
-
-// Cron job to stay updated to Tweets
-const job = new CronJob({
-  cronTime: "*/5 * * * *",
-  onTick: async function () {
-    try {
-      const res = await getTweets()
-      config.app.chatIdArr.forEach(sendTweet)      
-    } 
-    catch (err) { console.error(err) }      
-  },
-  runOnInit: false,
-  timeZone: 'America/Caracas'
-});
-
-// Start Cron job
-job.start();
-
-// Save new config file values
-const saveConfigFile = () => {
-  jsonFile.writeFileSync("./config.json", config);
-  console.log('config file saved')
-}
-// Start command handler
-const handleStart = (chatId) => {
-  const text = `Welcome, type /help for a command list`
-
-  bot.sendMessage(chatId, text)
-  manageChats(chatId)
-  sendTweet(chatId)
-
-  Botan.track(msg, 'start')
-}
-// Stop command handler
-const handleStop = (chatId) => {
-  const text = `You have successfully unregistered, you won't receive any feed.
-  Use /start command to register again.`
-
-  bot.sendMessage(chatId, text)
-  manageChats(chatId, false)
-
-  Botan.track(msg, 'stop')
-}
-// Last Tweet command handler
-const handleLastTweet = (chatId) => {
-  sendTweet(chatId)
-  Botan.track(msg, 'lasttweet')
-}
+})
